e7bc5e6f0822b7f2a89a410c391c83110db485c3
tommaso auerbach <tsa302@nyu.edu>
2019-06-03 03:34:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,11 @@
-collection = ["ruby", "javascript", "python", "objective-c"]
+languages = ["ruby", "javascript", "python", "objective-c"]
 
 def my_collect(array)
   my_statments = []
   i = 0
 end
+
+def my_collect(students)
+
+  students.each do
+    puts
